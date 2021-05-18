e84bddc4893ae79a67c551f192743edaeb0b3b35
JavohirJR <j.javohir.dip@gmail.com>
2021-05-18 18:31:21 +0500

    pc-market-security | 18.05.2021 | second edition

--- src/main/java/com/javohir/task2/config/SecurityConfiguration.java
+++ src/main/java/com/javohir/task2/config/SecurityConfiguration.java
@@ -33,11 +33,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+
+        //        SUPER_ADMIN, MODERATOR va OPERATOR rollari bo’lsin.
+        //       ----- SUPER_ADMIN har qanday ishni qila oladi;
+        //        MODERATOR mahsulot qo’sha oladi va tahrirlay oladi ,ammo o’chira olmaydi;
+        //        OPERATOR buyurtmalar bilan ishlaydi, mahsulotni o'chira olmaydi va tahrirlay olmaydi.
+
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/api/**").hasAnyRole(Roles.MODERATOR.name(), Roles.SUPER_ADMIN.name(), Roles.OPERATOR.name())
-                .antMatchers(HttpMethod.PATCH, "/api/**").hasAnyRole(Roles.MODERATOR.name(), Roles.SUPER_ADMIN.name())
-                .antMatchers(HttpMethod.POST, "/api/**").hasAnyRole(Roles.MODERATOR.name(), Roles.SUPER_ADMIN.name())
+                .antMatchers(HttpMethod.GET, "/api/product/**").hasAnyRole(Roles.MODERATOR.name(), Roles.SUPER_ADMIN.name(), Roles.OPERATOR.name())
+                .antMatchers(HttpMethod.PATCH, "/api/product/**").hasAnyRole(Roles.MODERATOR.name(), Roles.SUPER_ADMIN.name())
+                .antMatchers(HttpMethod.POST, "/api/product/**").hasAnyRole(Roles.MODERATOR.name(), Roles.SUPER_ADMIN.name())
+                .antMatchers("/api/order/**").hasAnyRole(Roles.SUPER_ADMIN.name(), Roles.OPERATOR.name())
                 .antMatchers("/api/**").hasRole(Roles.SUPER_ADMIN.name())
                 .anyRequest().authenticated()
                 .and()
